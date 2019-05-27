fb6a5678840319e17a0c8e98b465493c9cd0712b
Jakub Kaczyński <jakc@netcompany.com>
2019-05-27 18:27:33 +0200

    commit before pull Office

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,18 +1,9 @@
 public class Main {
 
-
     public static void main(String[] args) {
-
-        System.out.println("Print text");
-        System.out.println("Print text 1");
-        System.out.println("123");
-
+        System.out.println("Main Office");
     }
 
-
-
-
-
-
-
 }
+
+
